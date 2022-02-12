27b4f7070baaaf3a5723e17d237c8f4caf0a350c
huangxuepeng <2695009886@qq.com>
2022-02-12 20:31:30 +0800

    接口测试

--- main.go
+++ main.go
@@ -16,41 +16,45 @@ type Person struct {
 func main() {
 	r := gin.Default()
 	r.Use(middleware.Cors())
-	r.POST("/test", test)
+	r.GET("/test", test)
 	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.Run(":8082")
+	r.Run(":8080")
 }
 
-// @summary test测试
-// @description 协同开发接口样式测试
-// @accept json
-// @produce json
-// @success 200 {string}
-// @param id body int true "下面填写用户id"
-// @param name body string true "填写姓名"
-// @router /test [post]
 func test(c *gin.Context) {
-	var person Person
-	if err := c.ShouldBind(&person); err != nil {
-
-		c.JSON(http.StatusOK, gin.H{
-
-			"Success": false,
-			"code":    500,
-			"message": "提交失败",
-			"error":   err.Error(),
-			"data": gin.H{
-				"user": "",
-			},
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"Success": true,
-		"code":    200,
-		"message": "提交成功",
-		"data": gin.H{
-			"user": "巴拉巴拉",
-		},
-	})
+	c.JSON(http.StatusOK, "你好")
 }
+
+// // @summary test测试
+// // @description 协同开发接口样式测试
+// // @accept json
+// // @produce json
+// // @success 200 {string}
+// // @param id body int true "下面填写用户id"
+// // @param name body string true "填写姓名"
+// // @router /test [post]
+// func test(c *gin.Context) {
+// 	var person Person
+// 	if err := c.ShouldBind(&person); err != nil {
+
+// 		c.JSON(http.StatusOK, gin.H{
+
+// 			"Success": false,
+// 			"code":    500,
+// 			"message": "提交失败",
+// 			"error":   err.Error(),
+// 			"data": gin.H{
+// 				"user": "",
+// 			},
+// 		})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"Success": true,
+// 		"code":    200,
+// 		"message": "提交成功",
+// 		"data": gin.H{
+// 			"user": "巴拉巴拉",
+// 		},
+// 	})
+// }
